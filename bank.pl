7b3c4f0a200cb08bcda5b8015139a5c694d4e6aa
caymanIslander <duman.ozgur@hotmail.com>
2024-03-08 15:31:11 +0300

    sender and receiver balance calculations

--- bank.pl
+++ bank.pl
@@ -14,17 +14,19 @@ client(2, '22222222222', 'Jane', 'Doe', 'F').
 % Rule for EFT
 eft(SenderIban, ReceiverIban, Amount) :-
     account(SenderAccount, _, SenderIban, _, SenderBalance),
-    account(ReceiverAccount, _, ReceiverIban, _, _),
+    account(ReceiverAccount, ReceiverBank, ReceiverIban, ReceiverClient, ReceiverBalance),
     SenderBalance >= Amount,
     NewSenderBalance is SenderBalance - Amount,
+    NewReceiverBalance is ReceiverBalance + Amount,
     retract(account(SenderAccount, SenderBank, SenderIban, SenderClient, SenderBalance)),
     assert(account(SenderAccount, SenderBank, SenderIban, SenderClient, NewSenderBalance)),
+    retract(account(ReceiverAccount, ReceiverBank, ReceiverIban, ReceiverClient, ReceiverBalance)),
+    assert(account(ReceiverAccount, ReceiverBank, ReceiverIban, ReceiverClient, NewReceiverBalance)),
     write('EFT transaction is successful.'),
     write('Sender Balance:'),
-    write(SenderBalance),
+    write(NewSenderBalance),
     write('Receiver Balance:'),
-    write(ReceiverBalance).
-    
+    write(NewReceiverBalance).    
 
 % Rule for transfer
 transfer(SenderAccountNumber, ReceiverName, ReceiverSurname, Amount) :-
