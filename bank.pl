f6547a34f528e708c9588cb77da18b70f2eb8a57
caymanIslander <duman.ozgur@hotmail.com>
2024-03-12 14:15:30 +0300

    Fee functions

--- bank.pl
+++ bank.pl
@@ -7,6 +7,7 @@ bank(2, 'Bank B').
 % Facts about accounts
 % account(accountNumber, bankID, IBAN, clientID, balance)
 account(1001, 1, 'TR10010001', 1, 5000).
+account(1003, 1, 'TR10030001', 3, 500).
 account(1002, 2, 'TR10020001', 2, 8000).
 
 % Facts about clients
@@ -14,22 +15,23 @@ account(1002, 2, 'TR10020001', 2, 8000).
 client(1, '11111111111', 'John', 'Doe', 'M').
 client(2, '22222222222', 'Jane', 'Doe', 'F').
 
+
 % Rule for EFT
 eft(SenderIban, ReceiverIban, Amount) :-
-    account(SenderAccount, _, SenderIban, _, SenderBalance),
+    account(SenderAccount, SenderBank, SenderIban, SenderClient, SenderBalance),
     account(ReceiverAccount, ReceiverBank, ReceiverIban, ReceiverClient, ReceiverBalance),
-    SenderBalance >= Amount,
-    NewSenderBalance is SenderBalance - Amount,
+    (SenderBank = ReceiverBank -> Fee = 0 ; Fee = 5), % Set fee to 5 if banks are different
+    SenderBalance >= Amount + Fee,
+    NewSenderBalance is SenderBalance - Amount - Fee,
     NewReceiverBalance is ReceiverBalance + Amount,
     retract(account(SenderAccount, SenderBank, SenderIban, SenderClient, SenderBalance)),
-    assert(account(SenderAccount, SenderBank, SenderIban, SenderClient, NewSenderBalance)),
+    assertz(account(SenderAccount, SenderBank, SenderIban, SenderClient, NewSenderBalance)),
     retract(account(ReceiverAccount, ReceiverBank, ReceiverIban, ReceiverClient, ReceiverBalance)),
-    assert(account(ReceiverAccount, ReceiverBank, ReceiverIban, ReceiverClient, NewReceiverBalance)),
-    write('EFT transaction is successful.'),
+    assertz(account(ReceiverAccount, ReceiverBank, ReceiverIban, ReceiverClient, NewReceiverBalance)),
     write('Sender Balance:'),
     write(NewSenderBalance),
     write('Receiver Balance:'),
-    write(NewReceiverBalance).    
+    write(NewReceiverBalance).
 
 % Rule for transfer
 transfer(SenderAccountNumber, ReceiverName, ReceiverSurname, Amount) :-
